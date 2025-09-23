d7863303772c5383ba9c0f7f6d382b5d6d0d4101
renan823 <renan.trofino@gmail.com>
2025-09-23 15:16:26 -0300

    feat: database setup and migrations

---
+++ backend/pkg/database/database.go
@@ -0,0 +1,27 @@
+package database
+
+import (
+	"codelab/backend/pkg/config"
+	"fmt"
+
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
+
+/*
+Cria uma conexão com o banco de dados via GORM.
+Recebe as variáveis da conexão via struct config.
+*/
+func ConnectDatabase(config *config.Config) (*gorm.DB, error) {
+	dsn := fmt.Sprintf(
+		"host=%s port=%s user=%s password=%s dbname=%s sslmode=%s",
+		config.Postgres.Host,
+		config.Postgres.Port,
+		config.Postgres.User,
+		config.Postgres.Password,
+		config.Postgres.Database,
+		"disable",
+	)
+
+	return gorm.Open(postgres.Open(dsn), &gorm.Config{})
+}
---
+++ backend/pkg/migrations/migrations.go
@@ -0,0 +1,13 @@
+package migrations
+
+import "gorm.io/gorm"
+
+/*
+Aplica as migrações (criação / atualização]
+de tabelas) no banco utilizando as structs
+de modelos definidas no domain.
+Para cada struct (entidade) uma tabela é gerada.
+*/
+func ApplyMigrations(db *gorm.DB) error {
+	return db.AutoMigrate()
+}
